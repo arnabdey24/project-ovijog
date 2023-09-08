cc672d3091ad5fb32b0449f18bd46c04ecb0746b
Arnab <arnab.dey@bracits.com>
2023-09-08 23:50:30 +0600

    -config changes

--- app/config/config.php
+++ app/config/config.php
@@ -6,6 +6,6 @@ define("DB_PASS", "root");
 define("DB_NAME", "ovhijog");
 
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', 'http://localhost/ovijog');
+define('URLROOT', 'https://project-ovijog.azurewebsites.net');
 define('SITENAME', 'Project Ovijog');
 define('APPVIRSION', '1.0.0');
